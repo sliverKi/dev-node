a4d37636a9349ccee8f1eea25a77f3054028f913
sliverKi <lovee2756@gmail.com>
2023-12-13 18:22:57 +0900

    how to routing use middleware

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     console.log("In the middleware");
     //res.send("<h2>First useFunction</h2>");
     req.message = "<h2>First useFunction</h2>"
@@ -14,13 +13,26 @@ app.use((req, res, next) => {
     const fullMessage = req.message + "<h2>Second useFunction</h2>"
     res.send(fullMessage);
 });
-//use(): 새로운 미들웨어 함수 추가 가능
-//send(): 서버에 응답을 보낼 수 있음 ~> write()와 사용 비슷
-//기본 응답 헤더를 text/html로 자동으로 default설정 해줌, 
-//send() + write()를 혼용하여 사용 할 수 없음
+*/
+
+app.use('/add-product', (req, res, next) => { 
+    console.log("In another middleware")
+    res.send('<h2>The Add-product</h2>')
+})
+
+app.use('/', (req, res, next) => { 
+    console.log("In another middleware")
+    res.send('<h2>Second middleware</h2>')
+})
 
 
 app.listen(3000)
 /*shortcut: app.listen(3000)
 const server = http.createServer(app);
 server.listen(3000);*/
+
+//use(): 새로운 미들웨어 함수 추가 가능
+//send(): 서버에 응답을 보낼 수 있음 ~> 둘다 무언가를 전송한다는 점에서 write()와 사용 비슷
+//send()는 end()와 동일한 기능을 수행함
+//기본 응답 헤더를 text/html로 자동으로 default설정 해줌, 
+//send() + write()를 혼용하여 사용 할 수 없음
