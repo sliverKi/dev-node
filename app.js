f0329e19b77937370b4b3abd89e427a0e8cf7906
sliverKi <lovee2756@gmail.com>
2023-12-13 19:00:45 +0900

    Filter GET method when user approach /product url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
 })
 
-app.use('/product', (req, res, next) => { 
+app.post('/product', (req, res, next) => { 
     console.log(req.body)//사용자가 보낸 내용 추출~> undefined가 출력이 돼는 이유: req.은 들어오는 요청 본문을 따로 분석하려 하지 않음
     res.redirect('/')
 })
