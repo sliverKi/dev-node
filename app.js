6a10af8ca9d153a9222c63a337ccbef7c8c41171
sliverKi <lovee2756@gmail.com>
2023-12-14 15:37:12 +0900

    Add route Filtering

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes)
-app.use(shopRoutes)
+app.use('/admin', adminRoutes)//outSourcing-Routing
+app.use(shopRoutes)//outSourcing-Routing
 
 app.use((req, res, next) => { 
-    res.statusCode = 404
+    statusCode = 404
     res.status(statusCode).send('<h1>404 Page NotFound</h1>')
 })
 app.listen(3000)
 
+/* outSourcing-Routing
+
+시작경로를 공유하는 경우가 있다~> 
+/admin/add-product  admin/*시작 라우티 경로 동일 
+/admin/product
+*/
--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/add-product', (req, res, next) => { 
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
 })
 
-router.post('/product', (req, res, next) => { 
+router.post('/add-product', (req, res, next) => { 
     console.log(req.body)//사용자가 보낸 내용 추출~> undefined가 출력이 돼는 이유: req.은 들어오는 요청 본문을 따로 분석하려 하지 않음
     res.redirect('/')
 })
 
 module.exports = router
+
+/* outSourcing-Routing
+'/add-product' == '/admin/add-product'(GET Method)
+'/product' => '/admin/add-product'로 변경 가능(POST Method) 
+why~> router.X 처리하는 http method가 다르기 때문 => http Method가 다르면, 서로 같은 경로를 이용할 수 있음
+*/
