31d40354ff7d9464e5bc5fea874ecf17b0312823
sliverKi <lovee2756@gmail.com>
2023-12-15 21:55:48 +0900

    Change response object to pug file when user add product.

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,10 @@ const rootDir = require('../util/path')
 
 const products = []
 router.get('/add-product', (req, res, next) => { 
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', {docTitle: 'Add-Product-Pug'})
+   
+
 
 })
 
