29ddbf8e48cfd6031f18ce9337bf037ba655922d
sliverKi <lovee2756@gmail.com>
2023-12-15 21:48:44 +0900

    Change response object to pug file when user approach notFound

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(shopRoutes)//outSourcing-Routing
 
 app.use((req, res, next) => { 
    
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-notFound.html'))
+    //res.status(404).sendFile(path.join(__dirname, 'views', 'page-notFound.html'))
+    res.render('page-notFound', {docTitle: 'NotFound'})//shop template rendering
 
 })
 app.listen(3000)
