a762985799476734eb37f7e16d246eac0142de6d
sliverKi <lovee2756@gmail.com>
2023-12-15 17:25:52 +0900

    Set template engine Pug change to render() pug file

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express();
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,9 @@ const router = express.Router()
 const rootDir = require('../util/path')
 const adminData = require('./admin')
 router.get('/', (req, res, next) => { 
-    console.log(adminData.products)
-    res.sendFile(path.join(rootDir, 'views','shop.html'))
+    //console.log(adminData.products)
+    //res.sendFile(path.join(rootDir, 'views','shop.html'))
+    res.render('shop')
 })
 
 module.exports = router
