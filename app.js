fec93ef32e849b2478aafd4e59b73be7a03b4e83
sliverKi <lovee2756@gmail.com>
2023-12-16 22:42:56 +0900

    Change view-engine to ejs

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const expressHbs = require("express-handlebars");
+
 
 const app = express();
-app.engine(
-	"handlebars",
-	expressHbs({
-		layoutsDir: "views/layouts",
-		defaultLayout: "main-layout",
-        extname: "handlebars",
-        layout: false,
-	})
-);
+// app.engine(
+// 	"handlebars",
+// 	expressHbs({
+// 		layoutsDir: "views/layouts",
+// 		defaultLayout: "main-layout",
+//         extname: "handlebars",
+//         layout: false,
+// 	})
+// );
 //extname: 파일 경로의 확장자명을 설정하여, node engine에게 알림
 //if not set extname~> express doesn't know which file choose...
 
 //내장되어 있지 않은 template engine사용 <-> pug:내장 template-engine
-app.set("view engine", "handlebars");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
