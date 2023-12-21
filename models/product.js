9c40348d309fd8995f7a78297b323f0b16e068b0
sliverKi <lovee2756@gmail.com>
2023-12-21 22:09:13 +0900

    Comment: add comment 'this' pointing

--- models/product.js
+++ models/product.js
@@ -3,6 +3,7 @@ const products = [];
 module.exports = class Product { 
     constructor(t) { //매개변수 : title 
         this.title = t //this.title을 인수로 수신함
+        //this == Product 객체를 가리킴
     }
     save() { 
         products.push(this)
