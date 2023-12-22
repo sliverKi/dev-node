eed631c526293b6698e4092b37426fe1492c2917
sliverKi <lovee2756@gmail.com>
2023-12-23 05:21:07 +0900

    Modify: const p variable global, add getProductsFromFile to common code into a function

--- models/product.js
+++ models/product.js
@@ -2,6 +2,23 @@
 
 const fs = require("fs"); //node core module import
 const path = require("path"); //경로 구축
+
+const p = path.join(
+    path.dirname(require.main.filename),
+    "data",
+    "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+			cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+	});
+};
+
 module.exports = class Product {
 	constructor(t) {
 		//매개변수 : title
@@ -10,34 +27,14 @@ module.exports = class Product {
 	}
 	save() {
 		//products.push(this)
-		const p = path.join(
-			path.dirname(require.main.filename),
-			"data",
-			"products.json"
-        );
-        fs.readFile(p, (err, fileContent) => { 
-            let products = [];
-            if (!err) { 
-                products = JSON.parse(fileContent)
-            }
-            products.push(this)
-            fs.writeFile(p, JSON.stringify(products), (err) => { 
+        getProductsFromFile((products) => { 
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => { 
                 console.log(err)
             })
-        } )
-	}
-    static fetchAll(cb) {
-        const p = path.join(
-			path.dirname(require.main.filename),
-			"data",
-			"products.json"
-        );
-        fs.readFile(p, (err, fileContent) => { 
-            if (err) { 
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
         })
-		//return products;
+	}
+	static fetchAll(cb) {
+		getProductsFromFile(cb)
 	}
 };
