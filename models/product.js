79e47fe8af4a6460ca89a7cf1afccbf628bc4501
sliverKi <lovee2756@gmail.com>
2023-12-22 23:53:57 +0900

    Save data provide by a user into a json file, but during this process an error is encounter

--- models/product.js
+++ models/product.js
@@ -14,7 +14,17 @@ module.exports = class Product {
 			path.dirname(require.main.filename),
 			"data",
 			"products.json"
-		);
+        );
+        fs.readFile(p, (err, fileContent) => { 
+            let products = [];
+            if (!err) { 
+                products = JSON.parse(fileContent)
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => { 
+                console.log(err)
+            })
+        } )
 	}
 	static fetchAll() {
 		return products;
