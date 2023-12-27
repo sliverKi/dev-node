cf687d0046fc901c0f45cdd1872fc6bad466919a
sliverKi <lovee2756@gmail.com>
2023-12-27 16:06:29 +0900

    Modify: view data sent by the controller from product-list and index view

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,11 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
 	//products.push({ title: req.body.title });//model에 이미 정의 했기 때문에 더이상 controller에서 필요가 없어짐 
-	const product = new Product(req.body.title)//model에 정의한 class Product을 이용하여 객체를 생성함, keyword : new + class 
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const description = req.body.description;
+	const price = req.body.price;
+	const product = new Product(title, imageUrl, description, price)//model에 정의한 class Product을 이용하여 객체를 생성함, keyword : new + class 
     product.save()//생성환 객체를 save method를 통해, 배열에 저장
     res.redirect("/");
 }
