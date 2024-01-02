3cf8a30937d832f4567c46bfaf84cdb96d960d9f
sliverKi <lovee2756@gmail.com>
2024-01-02 18:06:26 +0900

    Import db module: make table, excute query and connect promise result use  then and catch block

--- app.js
+++ app.js
@@ -2,25 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const db = require('./util/database')
+const db = require("./util/database");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/error")
+const errorController = require("./controllers/error");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-db.execute('SELECT * FROM product')
+db.execute("SELECT * FROM product")
+	.then((result) => {
+		console.log(result);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRoutes); 
-app.use(shopRoutes); 
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 app.listen(3000);
-
-
